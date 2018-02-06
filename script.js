f5612213e36bbe82261ac2cf066dbe4a3c3d6982
Nick Johnson <njohnson7@users.noreply.github.com>
2018-02-05 21:13:39 -0800

    finished dynamic animations project

--- script.js
+++ script.js
@@ -1,67 +1,65 @@
 $(function () {
-  const SHAPE_TYPES = ['shape', 'circle', 'star'];
-  const SHAPE_SIZE  = 30;
-  const MOVE_SPEED  = 800;
-  const $canvas     = $('.canvas');
-
-  const createShape = (type, { startX, startY, endX, endY }) => (
-    $('<div></div>')
-      .addClass(`shape ${type}`)
-      .css({
-        left: startX,
-        top:  startY,
-      }).data({
-        startX,
-        startY,
-        endX,
-        endY,
-      })
-  );
-
-  HTMLDivElement.prototype.move = function (speed = MOVE_SPEED) {
-    let $shape = $(this);
-    if (!$shape.hasClass('shape')) return;
-
-    $shape.css({
-      left: $shape.data('startX'),
-      top:  $shape.data('startY'),
-    }).animate({
-      left: $shape.data('endX'),
-      top:  $shape.data('endY'),
-    }, speed);
-  };
-
-  const randomCoord  = max => ~~(Math.random() * max + 1);
-  const isValidCoord = (coord, max) => coord && coord >= 0 && coord <= max;
-  const clampCoord   = (coord, max) => isValidCoord(coord, max) ? +coord : randomCoord(max);
-
-  const getCoords = _ => {
-    let [startX, startY, endX, endY] = $('.coords input').map((_, coord) => $(coord).val() || NaN);
-    [startX, endX] = [startX, endX].map(coord => clampCoord(coord, $canvas.width()  - SHAPE_SIZE));
-    [startY, endY] = [startY, endY].map(coord => clampCoord(coord, $canvas.height() - SHAPE_SIZE));
-    return { startX, startY, endX, endY };
-  };
+  const $canvas      = $('.canvas');
+  const $form        = $('form');
+  const $inputCoords = $('.coords input');
+  const SHAPE_TYPES  = ['shape', 'circle', 'star'];
+  const SHAPE_SIZE   = 30;
+  const MAX_WIDTH    = $canvas.width()  - SHAPE_SIZE;
+  const MAX_HEIGHT   = $canvas.height() - SHAPE_SIZE;
+  const MIN_SPEED    = 100;
+  const MAX_SPEED    = 3000;
 
   const randomType = _ => SHAPE_TYPES[~~(Math.random() * SHAPE_TYPES.length)];
   const getType    = _ => $(':checked').val() || randomType();
 
-  $canvas.addShape = function () {
-    let type   = getType();
-    let coords = getCoords();
-    createShape(type, coords).appendTo(this).hide().show('fast');
+  const randomCoord  = max           => ~~(Math.random() * max + 1);
+  const isValidCoord = (coord, max)  => coord && coord >= 0 && coord <= max;
+  const clampCoord   = (coord, max)  => isValidCoord(coord, max) ? +coord : randomCoord(max);
+  const clampCoords  = (coords, max) => coords.map(coord => clampCoord(coord, max));
+  const getCoords    = function () {
+    let [startX, startY, endX, endY] = $inputCoords.map((_, coord) => $(coord).val() || NaN);
+    [startX, endX] = clampCoords([startX, endX], MAX_WIDTH);
+    [startY, endY] = clampCoords([startY, endY], MAX_HEIGHT);
+    return { startX, startY, endX, endY };
   };
 
-  $('form').submit(function (e) {
+  const randomSpeed = _ => ~~(Math.random() * MAX_SPEED - MIN_SPEED + 1) + MIN_SPEED;
+  const getSpeed    = _ => +$('.speed input').val().trim() || randomSpeed();
+
+  const createShape = (type, { startX, startY, endX, endY }, speed) => ($('<div>', {
+    class: `shape ${type}`,
+    data:  { startX, startY, endX, endY, speed },
+    on:    {
+      reset() {
+        $(this).css({ left: startX, top: startY });
+      },
+      move() {
+        $(this).trigger('reset').animate({ left: endX, top: endY }, speed);
+      },
+    },
+  }).trigger('reset'));
+
+  $canvas.addShape = function () {
+    createShape(getType(), getCoords(), getSpeed()).appendTo(this).hide().show('fast');
+  };
+
+  const move  = (_, shape) => $(shape).trigger('move');
+  const reset = (_, shape) => $(shape).trigger('reset');
+
+  $form.submit(function (e) {
     e.preventDefault();
     $canvas.addShape();
-    $(this).trigger('reset');
+    this.reset();
   });
 
   $('a').click(function (e) {
     e.preventDefault();
     let $control = $(this);
     let $shapes  = $canvas.children().stop();
-    if      ($control.is('.start')) $shapes.each((_, shape) => shape.move());
+    $form.trigger('reset');
+
+    if      ($control.is('.start')) $shapes.each(move);
+    else if ($control.is('.reset')) $shapes.each(reset);
     else if ($control.is('.clear')) $shapes.remove();
   });
 });
